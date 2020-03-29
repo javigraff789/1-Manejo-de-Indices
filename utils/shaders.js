38ae69730db931bd3d0dfb1513bd254561491e77
Agustin Burset <agustin.burset@icloud.com>
2020-03-29 16:52:29 -0300

    Update shaders with color support; change branch name

--- utils/shaders.js
+++ utils/shaders.js
@@ -1,8 +1,12 @@
 export const vertexShaderSourceCode = `#version 300 es
 
   in vec2 vertexPosition;
+  in vec3 vertexColor;
+
+  out vec3 color;
 
   void main() {
+    color = vertexColor;
     gl_Position = vec4(vertexPosition, 0, 1);
   }
 `
@@ -10,9 +14,11 @@ export const vertexShaderSourceCode = `#version 300 es
 export const fragmentShaderSourceCode = `#version 300 es
   precision mediump float;
 
+  in vec3 color;
+
   out vec4 fragmentColor;
 
   void main() {
-    fragmentColor = vec4(0, 1, 0, 1);
+    fragmentColor = vec4(color, 1);
   }
 `
